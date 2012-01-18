7c2a6d8ab462d45dcc1da85a64a4f47850771c66
Maxime Santerre <mooxeh@gmail.com>
2012-01-18 00:26:04 -0400

    Added level loading with proper math and lighting

---
+++ stats.js/utils/builder.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+rev = 8;
+output = '../build/Stats.js';
+string = "// stats.js r" + str(rev) + " - http://github.com/mrdoob/stats.js\n";
+
+os.system("java -jar compiler/compiler.jar --language_in=ECMASCRIPT5 --js ../src/Stats.js --js_output_file ../build/Stats.js");
+
+src_file = open(output,'r');
+string += src_file.read() + "\n";
+
+dep_file = open(output,'w');
+dep_file.write(string);
+dep_file.close();
